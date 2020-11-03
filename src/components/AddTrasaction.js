bf9cec5c6146b4647ce0dd09dc19422748c8f293
Amit Arya <somethingprogramed@gmail.com>
2020-11-03 22:23:28 +0530

    completed expense tracker

--- src/components/AddTrasaction.js
+++ src/components/AddTrasaction.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const AddTrasaction = () => {
+    const [text, setText] = useState('')
+    const [amount, setAmount] = useState(0)
+
+
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 1000000000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction)
+    }
     return (
         <>
             <h3>Add new Trasaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text..." />
-                </div> 
+                    <input type="text" value={text} onChange={e => setText(e.target.value)} placeholder="Enter text..." />
+                </div>
                 <div className="form-control">
                     <label htmlFor="amount">Amount
-                    <br /> (negative - expense, positive - income) 
+                    <br /> (negative - expense, positive - income)
                     </label>
-                    <input type="text" placeholder="Enter text..." />
-                </div> 
-                <button className="btn">Add Transaction</button>
- 
+                    <input type="text" value={amount} onChange={e => setAmount(e.target.value)} placeholder="Enter text..." />
+                </div>
+                <button className="btn" >Add Transaction</button>
+
             </form>
         </>
     );
--- src/components/transactions.js
+++ src/components/transactions.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Transactions = ({ transaction }) => {
 
     const sign = transaction.amount < 0 ? '-' : '+';
+    const { deleteTransaction } = useContext(GlobalContext);
 
     return (
         <li className={sign === '-' ? 'minus' : 'plus'}>
             {transaction.text} <span> {sign}${Math.abs(transaction.amount)}</span>
-            <button className="delete-btn">X</button>
+            <button className="delete-btn" onClick={() => deleteTransaction(transaction.id) }>X</button>
         </li>
     );
 }
